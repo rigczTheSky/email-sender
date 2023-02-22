398d76d65b9ab0b9d04feca01db7f7cc35565f84
rigczTheSky <tmarszelewski@gmail.com>
2023-02-22 23:21:41 +0100

    default log added

--- src/test/java/com/tmarszelewski/emailsender/controller/ContactControllerTest.java
+++ src/test/java/com/tmarszelewski/emailsender/controller/ContactControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
